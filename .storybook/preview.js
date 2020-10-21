5aaee8991572cebf20eacbe4a75ce0a0d8bed9ca
cfuller <cfuller@overstock.com>
2020-10-21 17:38:04 +0100

    Add GlobalStyle to app and stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../src/GlobalStyle';
 
+addDecorator(s => <><GlobalStyle />{s()}</>);
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import { GlobalStyle } from './GlobalStyle';
 
 const App = () => {
   return (
     <div className="App">
+        <GlobalStyle />
       Add components here
     </div>
   );
