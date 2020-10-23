2010e710287d176f505d88d5422bb3e7422c1a5d
cfuller <cfuller@overstock.com>
2020-10-23 10:23:48 +0100

    Fetch orders on App component mount, proxy requests to localhost:3001

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { GlobalStyle } from './GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 import { Theme } from './Theme';
+import OrderBook from './components/OrderBook/OrderBook';
+import { callFetch } from './helpers/call-fetch';
 
 const App = () => {
+    const [orders, setOrders] = useState({ buys: {}, sells: {} });
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setLoading(true);
+        setError(null);
+        callFetch('/book', { method: 'GET', headers: { 'Content-Type': 'application/json' }})
+            .then(({ response, error }) => {
+                if (error) {
+                    setError(error);
+                } else {
+                    setOrders(response);
+                }
+                setLoading(false);
+            });
+
+    }, []);
+
     return (
         <ThemeProvider theme={Theme}>
             <div className="App">
                 <GlobalStyle/>
-                Add components here
+                <OrderBook orders={orders} />
             </div>
         </ThemeProvider>
     );
