8d4f51280432515a820269b84b2d93bba120e00a
cfuller <cfuller@overstock.com>
2020-10-26 16:53:36 +0000

    Fix fallback font family name

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -2,6 +2,6 @@ import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
     body {
-        font-family: "Roboto", sans serif;
+        font-family: "Roboto", sans-serif;
     }
 `;
